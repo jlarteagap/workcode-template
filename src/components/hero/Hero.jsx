ef2f363410772720a346ff28f5f2b8fb7e0a89c0
Jorge <jorgelarteagap@gmail.com>
2022-03-25 02:27:00 -0400

    [FIX] mobile text css, and text from empresas/API

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import heroImg from '../../assets/top.jpg'
-import laptop from '../../assets/mac.png'
+import { getData } from '../../api/api'
+
 import './hero.css'
 export const Hero = () => {
+  const [empresa, setEmpresa] = useState('')
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const res = await getData('empresa')
+        setEmpresa(res.records)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
   return (
     <div className="row">
       <div className="container my-5">
         <div className="text-center">
           <img src={heroImg} alt="" className="img-fluid" width="900px" />
           <h1 className="wc__hero-title text-uppercase fs-1 fw-bolder">
-            Bienvenidos a workcore
+            Bienvenidos a {empresa.nombre}
           </h1>
         </div>
       </div>
       <div className="wc__hero-quote py-5">
-        <h2 className="fw-ligther text-center my-5 pb-5">
-          "<span className="fw-bolder">Nos adaptamos a tu empresa</span>,"{' '}
-          <br /> no tu empresa a nosotros
+        <h2 className="fw-ligther text-center my-5 pb-sm-0 pb-5">
+          <div dangerouslySetInnerHTML={{ __html: empresa.descripcion }} />
         </h2>
       </div>
       <div className="wc__hero-laptop-content position-relative">
@@ -24,8 +35,8 @@ export const Hero = () => {
       </div>
       <div className="row">
         <div className="d-flex justify-content-center my-5">
-          <h3 className="d-block fw-lighter">
-            sistema de planificación de recursos empresariales
+          <h3 className="d-block fw-lighter text-center">
+            <div dangerouslySetInnerHTML={{ __html: empresa.descripcion }} />
           </h3>
         </div>
       </div>
